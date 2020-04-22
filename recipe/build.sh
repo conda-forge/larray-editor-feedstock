f5a202426b9a026b78dc3f5379a19c0a3eda62eb
Gaëtan de Menten <gdementen@gmail.com>
2020-04-22 17:32:05 +0200

    updated v0.32 and modernize conda recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-mkdir -p "$PREFIX/Menu"
-if [ $OSX_ARCH ]
-then
-    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
-    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
-else
-    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
-    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
-fi
+# Ref: https://github.com/ContinuumIO/menuinst/wiki/Menus-in-Conda-Recipes
+# See https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html
+#     for a list of environment variables that are set during the build process.
 
-"$PYTHON" setup.py install --single-version-externally-managed --record record.txt || exit 1
+#mkdir -p "$PREFIX/Menu"
+#if [ $OSX_ARCH ]
+#then
+#    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
+#    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
+#else
+#    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
+#    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
+#fi
+
+"$PYTHON" -m pip install . --no-deps -vv || exit 1
