442dbcb3069c44d499ce07a6d72000b3942e8471
Gaëtan de Menten <gdementen@gmail.com>
2020-04-22 17:32:05 +0200

    updated v0.32 and modernize conda recipe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,12 @@
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
-:: Add more build steps here, if they are necessary.
+:: Ref: https://github.com/ContinuumIO/menuinst/wiki/Menus-in-Conda-Recipes
+:: See: https://docs.conda.io/projects/conda-build/en/latest/user-guide/environment-variables.html
+::      for a list of environment variables that are set during the build process.
+:: e.g. %PREFIX% = (...)\Miniconda
 
-:: https://github.com/ContinuumIO/menuinst/wiki/Menus-in-Conda-Recipes
 if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
 copy "%RECIPE_DIR%\larray-editor.json" "%PREFIX%\Menu"
 copy "%SRC_DIR%\larray_editor\images\larray.ico" "%PREFIX%\Menu"
 copy "%SRC_DIR%\larray_editor\images\larray-help.ico" "%PREFIX%\Menu"
 
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
+"%PYTHON%" -m pip install . --no-deps -vv
+if errorlevel 1 exit 1
